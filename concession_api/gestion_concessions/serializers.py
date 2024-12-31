0dc0d3eaf1734545dbdaa41affccbfb2d5a4b786
Fr33Ze06 <118484751+FreeZe060@users.noreply.github.com>
2024-12-31 15:19:23 +0100

    Serializers + Views

---
+++ concession_api/gestion_concessions/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Concession, Vehicule
+
+class ConcessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Concession
+        exclude = ['numero_siret']
+
+
+class VehiculeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicule
+        fields = '__all__'
--- concession_api/gestion_concessions/views.py
+++ concession_api/gestion_concessions/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Concession, Vehicule
+from .serializers import ConcessionSerializer, VehiculeSerializer
 
-# Create your views here.
+class ConcessionViewSet(viewsets.ModelViewSet):
+    queryset = Concession.objects.all()
+    serializer_class = ConcessionSerializer
+
+
+class VehiculeViewSet(viewsets.ModelViewSet):
+    queryset = Vehicule.objects.all()
+    serializer_class = VehiculeSerializer
